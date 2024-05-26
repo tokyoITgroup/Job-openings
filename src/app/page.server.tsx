6d160a7c717fee7649d7ba040119613ccf194fe3
dongmin7208 <dongmin.park.career@gmail.com>
2024-05-26 18:09:13 +0900

    FIX: backend_endpoint

--- src/app/page.server.tsx
+++
@@ -1,9 +0,0 @@
-// import fs from "fs";
-// import path from "path";
-
-// export const getJobPosts = () => {
-//   const filePath = path.join(process.cwd(), "./src/data/dummyData.json");
-//   console.log("filePath", filePath);
-//   const jsonData = fs.readFileSync(filePath, "utf8");
-//   return JSON.parse(jsonData);
-// };
