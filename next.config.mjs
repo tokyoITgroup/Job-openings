418973b8b7dd3c55235d000d3b660eeb2e173f20
dongmin7208 <dongmin.park.career@gmail.com>
2024-05-26 23:31:33 +0900

    FIX: next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
-  },
-  output: 'standalone',
+  output: 'export',
   async rewrites() {
     return [
       {
